0932dc4a286908791ebdc039c8f18084fdb4e8a7
RobinsonBerthet <104074261+RobinsonBerthet@users.noreply.github.com>
2022-04-21 15:02:40 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let toggleButton = document.getElementsByClassName('toggle-button')[0]// bouton trois traits
-let navbarLinks = document.getElementsByClassName('navbar-links')[0]//menu
+const toggleButton = document.getElementsByClassName('toggle-button')[0]// bouton trois traits
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]//menu
 const liens = document.querySelector('.navbar-links > ul > li > a');// rubriques(liens) du menu
-
+  
 
 toggleButton.addEventListener('click', () => {// quand on clique sur les trois traits pour faire apparaitre le menu 
   navbarLinks.classList.toggle('active');// la class navbarLinks change de nom pour faire apparaitre le menu grace au css
@@ -16,3 +16,45 @@ document.querySelectorAll('.navbar-links > ul > li > a').forEach((liens) => { //
       toggleButton.className= ('toggle-button');
     })
 });
+
+var btnPopup = document.getElementById('btnPopup');//recup id popup
+var overlay = document.getElementById('overlay');//recup id popup
+var btnClose = document.getElementById('btnClose');//recup id popup
+var popup = document.getElementById('popup');
+
+btnPopup.addEventListener('click', ouvrePopup);
+
+function ouvrePopup()
+{
+  overlay.style.display = "block";
+  body.style.overflow = 'hidden';
+}
+
+btnClose.addEventListener('click', fermePopup)
+
+function keyPress (e) {
+    if(e.key == 27) {
+      overlay.style.display = 'none';
+    }
+}
+
+function fermePopup()
+{
+  overlay.style.display = 'none';
+  body.style.overflow = 'scroll';
+}
+
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {
+        fermePopup();   
+    }
+};
+
